38f4b577b20780b75b558ad32ae203df462ade94
jugal <jsheoran@dminc.com>
2020-01-23 12:19:18 +0530

    1 st commit

---
+++ javscript/js/Second.js
@@ -0,0 +1,15 @@
+document.write(" Hello javascript","<br>");
+
+//document.getElementById("button").onclick ='alert("Button clicked");';
+
+function Demo(){
+    onclick=alert("this is alert");
+}
+function sorting(a,b){
+    return a-b;
+}
+
+let arr=[30,678,27,9,54685]
+document.write(arr)
+document.write("<br>",arr.sort(sorting))
+ 
